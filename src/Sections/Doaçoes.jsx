3d9b96b303d50ea0aeb8708d9e240d16ee4c24df
Gabriel <gabrielsouza65115@gmail.com>
2024-06-10 16:39:28 -0300

    Adicionando Vite

---
+++ src/Sections/Doaçoes.jsx
@@ -0,0 +1,9 @@
+import LabTabs from "../components/AccordionComponent";
+
+export default function Doações() {
+  return(
+    <div className="bg-white h-auto px-[5%]" id="doacoes">
+    <LabTabs />
+  </div>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        "color-fundo": "#D9D9D9",
+      },
+      screens: {
+        tela1: "320px",
+        tela2: "375px",
+        tela3: "425px",
+        tela4: "640px",
+        tela5: "768px",
+        tela6: "1024px",
+        tela7: "1280px",
+        tela8: "1440px",
+      },
+    },
+  },
+  plugins: [],
+};
